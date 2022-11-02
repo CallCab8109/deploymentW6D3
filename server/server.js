134dda8e84a421f4453fcd4f273d7c4c4d134f7a
Carter Bleyl <Carter.Bleyl@gmail.com>
2022-11-02 14:49:14 -0600

    css 1

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.get('/css', (req, res) => (
+    res.sendFile(path.join(__dirname, './public/main.css'))
+))
 
 const {home} = require("./controllers/pageCtrl.js")
 
